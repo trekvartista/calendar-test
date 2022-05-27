4bc809a45ece2db53305054e4b5f9e4403793fc7
trekvartista <isken_150502@mail.ru>
2022-05-27 23:59:32 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const App: FC = () => {
+    return (
+        <div>
+            <div className="text-3xl font-bold underline">App</div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const Calendar: FC = () => {
+    return (
+        <div>
+            Calendar
+        </div>
+    );
+};
+
+export default Calendar;
