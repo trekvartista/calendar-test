c8521122a72c620130cda9f14f27210332cd55a9
trekvartista <isken_150502@mail.ru>
2022-05-30 04:22:18 +0600

    Added date validator

--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,6 +1,17 @@
+import moment, { Moment } from "moment";
+
 export const rules = {
-    required: (message: string) => ({
+    required: (message: string = "This field is required.") => ({
         required: true,
         message
+    }),
+    isDateValid: (message: string = "Sorry, you cannot create an event for a past date.") => () => ({
+
+        validator(_: any, value: Moment) {
+            if (value.isSameOrAfter(moment())) {
+                return Promise.resolve();
+            }
+            return Promise.reject(new Error(message))
+        }
     })
 }
